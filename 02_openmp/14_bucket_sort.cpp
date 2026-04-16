d78a25776f7384eb39503dd7217ae42265b0962d
kumagai0212 <kumad0212@gmail.com>
2026-04-16 12:32:59 +0900

    first commit of homework in 02_openmp

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <vector>
+//#include <omp.h>
 
 int main() {
   int n = 50;
@@ -18,6 +19,9 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
+
+  //double start_time = omp_get_wtime();
+#pragma omp parallel for firstprivate(bucket)  
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
@@ -25,8 +29,13 @@ int main() {
     }
   }
 
+  //double end_time = omp_get_wtime();
+  //printf("Time: %f seconds\n", end_time - start_time);
+
   for (int i=0; i<n; i++) {
     printf("%d ",key[i]);
   }
   printf("\n");
+
+
 }
