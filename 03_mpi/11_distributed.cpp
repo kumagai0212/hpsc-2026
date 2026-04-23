3a187c6b7047585dd4e5f5cfef8d6d20ca1cc0a2
kumagai0212 <kumad0212@gmail.com>
2026-04-23 19:14:54 +0900

    first change of 11_distributed.cpp

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -26,9 +26,25 @@ int main(int argc, char** argv) {
   MPI_Datatype MPI_BODY;
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
+  MPI_Win win;
+  MPI_Win_create(jbody, (N/size)*sizeof(Body), sizeof(Body), MPI_INFO_NULL, MPI_COMM_WORLD, &win);
+  
+  Body jbody_send[N/size];
   for(int irank=0; irank<size; irank++) {
+/*
     MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  */
+
+    for(int i = 0; i < N/size; i++){
+        jbody_send[i] = jbody[i];
+    }
+
+
+    MPI_Win_fence(0, win);
+    MPI_Put(jbody_send, N/size, MPI_BODY, send_to, 0, N/size, MPI_BODY, win);
+    MPI_Win_fence(0, win);
+
     for(int i=0; i<N/size; i++) {
       for(int j=0; j<N/size; j++) {
         double rx = ibody[i].x - jbody[j].x;
@@ -41,6 +57,7 @@ int main(int argc, char** argv) {
       }
     }
   }
+
   for(int irank=0; irank<size; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if(irank==rank) {
